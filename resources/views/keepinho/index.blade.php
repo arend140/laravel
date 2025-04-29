9413852ffe602c26e19ac430bd2cda7042bd95f1
arend140 <brunoarend01@gmail.com>
2025-04-29 08:43:51 -0300

    Aula 3

--- resources/views/keepinho/index.blade.php
+++ resources/views/keepinho/index.blade.php
@@ -1,2 +1,16 @@
 <h1>Keepinho</h1>
 <p>Seja bem-vindo ao Keepinho, o seu assistente pessoal (melhor do que o Google).</p>
+<hr>
+<form action="{{ route('keep.gravar') }}" method="post">
+    @csrf
+    <textarea name="texto" id="" cols="30" rows="10"></textarea>
+    <br>
+    <input type="submit" value="Gravar nota">
+</form>
+
+
+@foreach ($notas as $nota)
+    <div style="border:1px dashed">
+        {{ $nota->texto }}
+    </div>
+@endforeach
